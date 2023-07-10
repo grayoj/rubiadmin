a426cfaf96a1dfd627122619ef4cd765e1d541ff
grayoj <Mgeraldoj07@gmail.com>
2023-07-10 09:05:17 +0100

    Fix constants and add session.

--- src/libs/Constants.ts
+++ src/libs/Constants.ts
@@ -1,4 +1,4 @@
 export const adminUrl = "https://rubidelivery.com:8443";
 export const appUrl = "https://api.rubidelivery.com:7443"
-export const dashboardUrl = "http://localhost:9443"
+export const dashboardUrl = "https://dashboard.rubidelivery.com:9443"
 export const avatarUrl = "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
